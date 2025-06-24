1d56e48091ec3eb7e366a068dc0ae92f26698a97
Ryannzx <ryanbrenno15@gmail.com>
2025-06-24 02:15:44 -0300

    Adições1

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,18 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Rota de teste
+app.get('/', (req, res) => {
+    res.send('API do Menos Fome Brasil está no ar!')
+})
+
 app.use('/campanhas', campanhaRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(5000, () => {
-            console.log('Servidor rodando em http://localhost:5000')
+        const PORT = process.env.PORT || 5000
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`)
         })
     })
     .catch(err => console.error('Erro ao conectar no MongoDB:', err))
